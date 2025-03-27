6e0e28d50af31a225a346154bbafffb41fb73c39
ARESOLTEC <consultores@aresoltec.com>
2025-03-27 12:08:13 +0000

    Add files via upload

--- login_user_detail/models/login_detail.py
+++ login_user_detail/models/login_detail.py
@@ -19,5 +19,18 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from . import login_detail
-from . import res_users
+from odoo import fields, models
+
+
+class LoginDetail(models.Model):
+    """ Class for the model login_detail contains fields related to
+     the model."""
+    _name = 'login.detail'
+    _description = 'Login Detail'
+
+    name = fields.Char(string="User Name", help="Name of logged in user")
+    date_time = fields.Datetime(
+        string="Login Date And Time",
+        default=lambda self: fields.datetime.now(),
+        help="Date and time of log in")
+    ip_address = fields.Char(string="IP Address", help="IP address of login")
