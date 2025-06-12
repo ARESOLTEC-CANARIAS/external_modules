302076679881c4a57c15e47f67d09f02b7abf667
ARESOLTEC <consultores@aresoltec.com>
2025-06-12 06:52:08 +0100

    Delete asterisk_pro_connect directory

--- asterisk_pro_connect/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2024 Manus AI
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl)
-
-from . import models
-from . import controllers
-from . import wizard
-from . import reports
-
--- asterisk_pro_connect/models/asterisk_channel.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2024 Manus AI
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl)
-
-from . import asterisk_event
-
