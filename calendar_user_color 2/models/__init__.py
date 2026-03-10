021e50a457dcc195c132445369326166c2539b3f
ARESOLTEC <consultores@aresoltec.com>
2026-03-10 16:36:15 +0000

    Delete calendar_user_color 2 directory

--- calendar_user_color 2/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import ir_http, calendar_event, calendar_filters
--- calendar_user_color 2/models/calendar_event.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class CalendarEvent(models.Model):
-    _inherit = "calendar.event"
-
-    color = fields.Integer(related="user_id.color")
